580004da0369f1a280f5d44a2d9f7d9079bcbc26
dark-shade <sr282828@gmail.com>
2018-06-07 16:23:48 -0500

    added central authority and ability to modify the coins in circulation

--- Radium.sol
+++ Radium.sol
@@ -1,21 +1,43 @@
 pragma solidity ^0.4.18;
 
-contract Radium{
+contract owned{
+    address public owner;
+    function owned(){
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function transferOwnership(address newOwner) onlyOwner{
+        owner = newOwner;
+    }
+}
+
+contract Radium is owned{
     string public name;
     string public symbol;
     uint8 public decimals;
+    uint256 public totalSupply;
     
     event Transfer(address indexed from, address indexed to, uint256 value);
     
     mapping (address => uint256) public balanceOf;
     
-    function Radium(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) public
+    function Radium(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address centralMinter) public
     {
-            balanceOf[msg.sender] = initialSUpply;
+            balanceOf[msg.sender] = initialSupply;
             
+            totalSupply = initialSupply;
             name = tokenName;
             symbol = tokenSymbol;
             decimals = decimalUnits;
+            if(centralMinter != 0)
+            {
+                owner = centralMinter;
+            }
     }
         
     function _transfer(address _from, address _to, uint256 _value) internal
@@ -30,5 +52,10 @@ contract Radium{
         emit Transfer(msg.sender, _to, _value);
     }
     
-    
+    fuction mintToken(address target, uint256 mintedAmount) onlyOwner{
+        balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        emit Transfer(0, owner, mintedAmount);
+        emit Transfer(owner, target, mintedAmount);
+    }
 }
