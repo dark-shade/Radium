88515146484a185d58ef68e3110fece4c6680e66
dark-shade <sr282828@gmail.com>
2018-06-07 18:00:44 -0500

    added rewards and proof of work

--- Radium.sol
+++ Radium.sol
@@ -21,10 +21,19 @@ contract Radium is owned{
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
+    uint256 public sellPrice;
+    uint256 public buyPrice;
+    uint public minBalanceForAccounts;
     
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    byte32 public currentChallenge;
+    uint public timeOfLastProof;
+    uint public difficulty = 10**32;
     
     mapping (address => uint256) public balanceOf;
+    mapping (address => bool) public frozenAccount;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event FrozenFunds(address target, bool frozen);
     
     function Radium(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address centralMinter) public
     {
@@ -38,6 +47,7 @@ contract Radium is owned{
             {
                 owner = centralMinter;
             }
+            timeOfLastProof = now;
     }
         
     function _transfer(address _from, address _to, uint256 _value) internal
@@ -50,12 +60,80 @@ contract Radium is owned{
         balaceOf[_to] += _value;
         
         emit Transfer(msg.sender, _to, _value);
+        
+        if(msg.sender.balance < minBalanceForAccounts)
+        {
+            sell((minBalanceForAccounts - msg.sender.balance) / sellPrice);
+        }
     }
     
-    fuction mintToken(address target, uint256 mintedAmount) onlyOwner{
+    function transfer(address _to, uint256 _value) public{
+        _transfer(msg.sender, _to, _value);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    
+    function mintToken(address target, uint256 mintedAmount) onlyOwner{
         balanceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
         emit Transfer(0, owner, mintedAmount);
         emit Transfer(owner, target, mintedAmount);
+    }
+    
+    function freezeAccount(address target, bool freeze) onlyOwner{
+        frozenAccount[target] = freeze;
+        emit FrozenFunds(target, freeze);
+    }
+    
+    function setPrices(unint256 newSellPrice, uint256 newBuyPrice) onlyOwner{
+        sellPrice = newSellPrice;
+        buyPrice = newBuyPrice;
+    }
+    
+    function buy() payable returns (uint amount){
+        amount = msg.value / buyPrice;
+        require(balanceOf[this] >= amount);
+        balanceOf[msg.sender] += amount;
+        balanceOf[this] -= amount;
+        emit Transfer(this, msg.sender, amount);
+        return amount;
+    }
+    
+    function sell(uint amount) returns (uint revenue){
+        require(balanceOf[msg.sender] >= amount);
+        balanceOf[this] += amount;
+        balanceOf[msg.sender] -= amount;
+        revenue = amount * sellPrice;
+        msg.sender.transfer(revenue);
+        emit Transfer(msg.sender, this, amount);
+        return revenue;
+    }
+    
+    function setMinBalance(uint minimumBalanceInFinney) onlyOwner{
+        minBalanceForAccounts = minimumBalanceInFinney * 1 finney;
+    }
+    
+    function giveBlockReward(){
+        balanceOf[block.coinbase] += 1;
+    }
+    
+    function proofOfWork(uint nounce){
+        bytes8 n = bytes8(sha3(nounce, currentChallenge));
+        require(n >= bytes8(difficulty));
+        
+        uint timeSinceLastProof = (now - timeOfLastProof);
+        require(timeSinceLastProof >= 5 seconds);
+        balanceOf[msg.sender] += timeSinceLastProof / 60 seconds;
+        
+        difficulty = difficulty * 10 minutes / timeSinceLastProof + 1;
+        
+        timeOfLastProof = now;
+        currentChallenge = sha3(nounce, currentChallenge, block.blockhash(block.number - 1));
     }
 }
